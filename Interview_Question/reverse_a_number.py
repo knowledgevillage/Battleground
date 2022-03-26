a6c3b53ab583885d0fb3eb18c090a9b9023bf61a
shubham tiwari <knowledgevillage1093@gmail.com>
2022-03-26 22:54:33 +0530

    practice session 2

---
+++ Interview_Question/reverse_a_number.py
@@ -0,0 +1,20 @@
+# take user input
+
+from imp import init_builtin
+
+
+
+
+
+#print(init_number)
+
+try:
+    init_number=int(input("Enter the number")) #take the input and convert that into integer
+    print(init_number)
+    reverse_number=0
+    while init_number !=0:
+        reverse_number=init_number%10+reverse_number*10
+        init_number=init_number//10
+    print("the reversed number is ",reverse_number)
+except ValueError:
+    print("PLease enter the number values from [0-9]")
