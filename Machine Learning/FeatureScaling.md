264cbd6d1fe6c586d60da46f74229d867bbc976c
shubham tiwari <knowledgevillage1093@gmail.com>
2022-11-20 19:05:32 +0530

    adding markdown files

---
+++ Machine Learning/FeatureScaling.md
@@ -0,0 +1,2 @@
+# Feature Scaling
+> Feature scaling is a method used to normalize the range of independent variables or features of data. In data processing, it is also known as data normalization and is generally performed during the data preprocessing step.
---
+++ Python_Basics/dictionary_1.py
@@ -0,0 +1,16 @@
+# When was Plato born?
+dict={"name": "Plato", "country": "Ancient Greece", "born": -427, "teacher": "Socrates", "student": "Aristotle"}
+
+#Type your answer below.
+answer_1=dict['born']
+
+print("Plato's age is {}" .format(answer_1))
+
+# change platos Birthday
+#Type your answer below.
+
+
+dict['born']="B.C 428"
+
+
+print(dict['born'])
