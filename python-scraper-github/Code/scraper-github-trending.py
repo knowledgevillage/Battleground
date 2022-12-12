b7d661492609d7b1d956a83272cd93931755dfae
shubham tiwari <knowledgevillage1093@gmail.com>
2022-12-12 11:19:30 +0530

    removed unwanted files

--- python-scraper-github/Code/scraper-github-trending.py
+++ python-scraper-github/Code/scraper-github-trending.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup as bs
 import csv
 import os
 
+#lets get current working directory
 cur_wrk_dir=os.getcwd()
-folder_name='python-scraper-github/data'
+folder_name='python-scraper-github/code/resultset'
 
 # Collect the github page page = requests.get('https://github.com/trending')
 page=requests.get('https://github.com/trending')
@@ -13,16 +14,15 @@ page=requests.get('https://github.com/trending')
 soup = bs(page.text, 'html.parser')
 #print(soup)
 
-
 # get the repo list
 repo_list = soup.find_all(class_="Box-row")
 
 
-print(len(repo_list))
+
 # find all instances of that class (should return 25 as shown in the github main page)
 #repo_list = repo.find_all(class_='Box-row')
+print(len(repo_list))
 
-#print(len(repo_list))
 # Open writer with name
 file_name = "github_trending_today.csv"
 file_path=os.path.abspath(os.path.join(cur_wrk_dir,folder_name,file_name))
@@ -36,25 +36,17 @@ f.writerow(['Developer', 'Repo Name', 'Number of Stars'])
 print("Lets see the final list")
 print('Developer \t repo_name \t no of stars')
 for repo in repo_list:
-    #print (repo)
-    #print(repo.find(class_="h3 lh-condensed").parent.text.strip())
+    
+    #let's itreate throgh each repository listed on the page 
     full_repo_list=repo.find(class_="h3 lh-condensed")
-    #check=repo.find(class_="h3 lh-condensed")
-    #print(type(full_repo_list))
-    #print(full_repo_list)
-    #developer=full_repo_list.span.string
-    repo_dtl=full_repo_list.a.text.strip() 
-    #print(develop)
-    #print('s')
-    #print(type(star))
-    #print(star.split('/'))
+    repo_dtl=full_repo_list.a.text.strip()
+
+    # lets find the repository information
     repo_info=repo_dtl.split('/')
     repo_devloper=repo_info[0].strip()
     repo_name=repo_info[1].strip()
 
-    #print(repo_devloper)
-    #print(repo_name)
-
+    
     # lets find the stars
     get_footer=repo.find(class_="f6 color-fg-muted mt-2")
     stars=get_footer.a.text.strip()
