4eb38aef96561b565219ad3da04dfdb023161396
knowledgevillage <knowledgevillage1093@gmail.com>
2024-01-14 16:27:51 +0530

    adding Java and R folder

---
+++ R_Basics/test.r
@@ -0,0 +1,38 @@
+# This is a test file for R sctiping
+
+4+5
+
+print("have you got result above as 9")
+
+print('Lets assign few varables to see there behaviour')
+
+a=3
+b=5
+print(a)
+sum=a+b
+print(sum)
+sub= a-b
+print(sub)
+div =a/b
+print(div)
+mul=a*b
+print(mul)
+
+
+dummy <- "Hellos r Programming"
+
+#this is a single line cooment
+
+demo1 <-2+5 ; demo2 <- 3/5
+demo1
+print(demo1)
+demo2
+print(demo2)
+sessionInfo()
+x<-1:10
+plot(x)
+?plot(x) # this is to get the help from R about a command or syntex
+# is a case senstive language
+a=10
+A=10
+
