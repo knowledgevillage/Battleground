11d86bacda862cc182e4925959955d5cc95ecaaf
knowledgevillage <knowledgevillage1093@gmail.com>
2024-02-14 10:54:33 +0530

    data strcutres in R.

---
+++ R_Programming/Data Structure/dataStructures.r
@@ -0,0 +1,18 @@
+#Data structures in R
+
+# vector
+# matrix
+# list
+# arrays
+# data frames
+
+# A vector is simply a list of items that are of the same type. To combine the list of items to a vector, use the c() function and separate the items by a comma.
+## elements of vactor are known as components
+# Type of vector can be check with typeof function
+
+
+# function in vector
+# length --> used to fined the number of elements in a vector
+# # type of Vector
+# 1 --> Atomic vector
+# 2 --> list
