245ff566934378c282b39ffab017a53ea5365368
knowledgevillage <knowledgevillage1093@gmail.com>
2024-03-15 10:56:14 +0530

    Package in flask

--- CodeColosseum/Flask/app.py
+++ CodeColosseum/Flask/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app =Flask(__name__)
 
-from controller import userController, productController  # always import after defining and initiating app aas flask firt of all serches for app
+#from controller import userController, productController  # always import after defining and initiating app aas flask firt of all serches for app
 
+#lets create a package and import all files
 
+from controller import *
 
 @app.route("/")
 def welcome():
