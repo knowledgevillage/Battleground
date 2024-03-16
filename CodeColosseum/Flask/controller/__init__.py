fe8876e61d2fc78372d095f0b98a166c0a3dfe5e
knowledgevillage <knowledgevillage1093@gmail.com>
2024-03-16 21:25:15 +0530

    importing files automatically by creating package

--- CodeColosseum/Flask/controller/__init__.py
+++ CodeColosseum/Flask/controller/__init__.py
@@ -1,1 +1,7 @@
-__all__ = ["userController", "productController"]
+import os 
+import glob
+
+__all__ = [os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__)+"/*.py")]
+
+#__all__ = ["userController", "productController"]
+
