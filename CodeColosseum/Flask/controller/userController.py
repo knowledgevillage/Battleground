fb9434d1804db6e44973b0fefc0a4c1e20480bf2
knowledgevillage <knowledgevillage1093@gmail.com>
2024-03-20 10:25:11 +0530

    CRUD in flask

--- CodeColosseum/Flask/controller/userController.py
+++ CodeColosseum/Flask/controller/userController.py
@@ -1,8 +1,24 @@
 from app import app
 from model.userModel import userModel
+from flask import request
 
 obj = userModel()
 
-@app.route("/user/signup")
-def userSignupController():
-    return obj.userSignupModel()
+@app.route("/user/getall")
+def userGetAllController():
+    return obj.userGetAllModel()
+
+
+@app.route("/user/addone", methods=["POST"])
+def userAddOneController():
+    return obj.userAddOneModel(request.form)
+
+
+@app.route("/user/update", methods=["PUT"])
+def userUpdateController():
+    return obj.userUpdateModel(request.form)
+
+
+@app.route("/user/delete/<id>", methods=["DELETE"])
+def userDeleteController(id):
+    return obj.userDeleteModel(id)
--- CodeColosseum/Flask/model/userModel.py
+++ CodeColosseum/Flask/model/userModel.py
@@ -1,3 +1,45 @@
+import mysql.connector
+import json
 class userModel():
-    def userSignupModel(self):
-        return "This is signup model field"
+    def __init__(self): # it's a contructor 
+         #connection Establishment code
+        try:
+            self.con=mysql.connector.connect(host="localhost", user="root", password="B@ttlef!ld$123", database="flaskbattlefield")
+            print("connection successful")
+            self.con.autocommit=True
+            self.cur=self.con.cursor(dictionary=True)
+
+        except:
+            print("some Error")
+        
+
+    def userGetAllModel(self):
+        self.cur.execute("select name, email, phone, role, password from usercontrol")
+        result=self.cur.fetchall()
+        print(result)
+        if len(result)>0:
+            return json.dumps(result)
+        else:
+            return "No Data Found"
+        
+    
+    
+    def userAddOneModel(self, data):
+        self.cur.execute(f"insert into usercontrol(name, email, phone, role, password) VALUES('{data['name']}', '{data['email']}', '{data['phone']}', '{data['role']}', '{data['password']}')")
+        #print(data)
+        return "User created successfully"
+    
+
+    def userUpdateModel(self, data):
+        self.cur.execute(f"update usercontrol set name='{data['name']}', email='{data['email']}' , phone='{data['phone']}' , role='{data['role']}' , password='{data['password']}' where id={data['id']} ")
+        if self.cur.rowcount>0:
+            return "User updated successfully"
+        else:
+            return "No thing to update"
+            
+    def userDeleteModel(self, data):
+        self.cur.execute(f"Delete from usercontrol where id ={data} ")
+        if self.cur.rowcount>0:
+            return "User Deleted successfully"
+        else:
+            return "No thing to delete"
