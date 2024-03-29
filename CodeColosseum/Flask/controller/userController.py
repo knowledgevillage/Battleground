07ac5729f04ede46816f0ae9d68eb34c4be7168c
knowledgevillage <knowledgevillage1093@gmail.com>
2024-03-29 14:03:21 +0530

    CRUD Opration upload and retrive file

--- CodeColosseum/Flask/controller/userController.py
+++ CodeColosseum/Flask/controller/userController.py
@@ -1,6 +1,7 @@
 from app import app
 from model.userModel import userModel
-from flask import request
+from flask import request, send_file
+from datetime import datetime
 
 obj = userModel()
 
@@ -26,3 +27,32 @@ def userDeleteController(id):
 @app.route("/user/patch/<id>", methods=["PATCH"])
 def userPatchController(id):
     return obj.userPatchModel(request.form,id)
+
+
+
+@app.route("/user/getall/limit/<limit>/page/<page>", methods=["GET"])
+def userPaginationController(limit, page):
+    return obj.userPaginationModel(limit, page)
+
+
+@app.route("/user/<uid>/upload/avtar", methods=["PUT"])
+def userUploadAvtarController(uid):
+    print(request.files)
+    file=request.files['avtar']
+    #file.save(f"/uploads/{file.filename}")
+    print(str(datetime.now().timestamp()).replace(".",""))
+    uniqueFileName=str(datetime.now().timestamp()).replace(".","")
+    print(file)
+    fileNameList=file.filename.split(".")
+    print(fileNameList)
+    fileExtention=fileNameList[-1]
+    print(fileExtention)
+    #file.save(f"/uploads/{file.filename}")
+    finalFilePath=f"/GitRepo/Battleground/CodeColosseum/Flask/uploads/{uniqueFileName}.{fileExtention}"
+    file.save(finalFilePath)
+    return obj.userUploadAvtarModel(uid, finalFilePath)
+
+
+@app.route("/upload/<filename>", methods=["GET"])
+def userGetAvtarController(filename):
+    return send_file(f"/GitRepo/Battleground/CodeColosseum/Flask/uploads/{filename}")
--- CodeColosseum/Flask/model/userModel.py
+++ CodeColosseum/Flask/model/userModel.py
@@ -1,8 +1,12 @@
 import mysql.connector
 import json
 from flask import make_response
+
+
 class userModel():
-    def __init__(self): # it's a contructor 
+
+ # defining a constructor to initialize objects/ connetion at the invocation of this call itself
+    def __init__(self): 
          #connection Establishment code
         try:
             self.con=mysql.connector.connect(host="localhost", user="root", password="B@ttlef!ld$123", database="flaskbattlefield")
@@ -14,6 +18,7 @@ class userModel():
             print("some Error")
         
 
+# Method to get all details
     def userGetAllModel(self):
         self.cur.execute("select * from usercontrol")
         result=self.cur.fetchall()
@@ -24,22 +29,24 @@ class userModel():
             return res
         else:
             return make_response({"message":"No Data Found"},204)
-        
-    
-    
+
+# Method to insert a record in the database  
     def userAddOneModel(self, data):
         self.cur.execute(f"insert into usercontrol(name, email, phone, role, password) VALUES('{data['name']}', '{data['email']}', '{data['phone']}', '{data['role']}', '{data['password']}')")
         #print(data)
         return make_response({"message":"User created successfully"},201)
-    
 
+
+# Method to update the record in database
     def userUpdateModel(self, data):
         self.cur.execute(f"update usercontrol set name='{data['name']}', email='{data['email']}' , phone='{data['phone']}' , role='{data['role']}' , password='{data['password']}' where id={data['id']} ")
         if self.cur.rowcount>0:
             return make_response({"message":"User updated successfully"},201)
         else:
             return make_response({"message":"No thing to update"},202)
-            
+
+
+# Method to delete record in database
     def userDeleteModel(self, data):
         self.cur.execute(f"Delete from usercontrol where id ={data} ")
         if self.cur.rowcount>0:
@@ -48,6 +55,7 @@ class userModel():
             return make_response({"message":"No thing to delete"},202)
         
 
+# Method to update a record with dynamic information
     def userPatchModel (self,data, id):
         qry="update usercontrol set "
         for key in data:
@@ -57,5 +65,35 @@ class userModel():
         self.cur.execute(qry)
         if self.cur.rowcount>0:
             return make_response({"message":"User updated successfully"},201)
+        else:
+            return make_response({"message":"No thing to update"},202)
+
+
+
+# Method to get all details in pagintation fasion
+    def userPaginationModel(self, limit, page):
+        limit=int(limit)
+        page=int(page)
+        start = (page*limit) - limit
+        print(f"start is {start}")
+        qry=f"select * from usercontrol limit {start} , {limit}"
+        self.cur.execute(qry)
+        result=self.cur.fetchall()
+        print(result)
+        if len(result)>0:
+            res=make_response({"payload":result, "page_no":page , "limit": limit},200)
+            res.headers['Access-Control-Allow-Origin']="*"
+            return res
+        else:
+            return make_response({"message":"No Data Found"},204)
+        
+
+            
+
+# Method to update the record in database
+    def userUploadAvtarModel(self, uid, finalFilePath):
+        self.cur.execute(f"update usercontrol set avtar ='{finalFilePath}' where id= {uid}")
+        if self.cur.rowcount>0:
+            return make_response({"message":"User uploaded successfully"},201)
         else:
             return make_response({"message":"No thing to update"},202)
